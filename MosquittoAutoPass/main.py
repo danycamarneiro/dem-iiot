19c82785712261e97bfd161aa11a9bbafe4568fe
danycamarneiro <daniel.camarneiro@ua.pt>
2025-02-07 12:43:21 +0000

    adding authentication v.alpha

---
+++ MosquittoAutoPass/main.py
@@ -0,0 +1,156 @@
+import docker
+import base64
+import json
+import paho.mqtt.client as mqtt
+
+#----------------------------------------------------------------------------
+def connect_mqtt(config):
+    def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(config["Host"], config["Port"])
+    return client
+#----------------------------------------------------------------------------
+def subscribe(client, config):
+    def on_message(client, userdata, msg):
+        message = msg.payload.decode()
+        message_json = json.loads(message)
+        update_auth(message_json,msg.topic)
+
+    client.subscribe(config["SubTopic"])
+    client.on_message = on_message
+#----------------------------------------------------------------------------
+def update_auth(message_json,mqtt_topic):
+    # print(message_json)
+    namespace_id = message_json['topic'].find("/")
+    device_id = message_json['topic'].find("/", namespace_id+1)
+    action = message_json['topic'][device_id+1:]
+    device = message_json['topic'][namespace_id+1:device_id]
+    
+    if action == "things/twin/events/modified": #update password
+        if "pass" in message_json['value']["attributes"]:
+            print("Modifying '" + mqtt_topic + "' password to '" + message_json['value']["attributes"]["pass"] +"'...")
+            update_device_broker(mqtt_topic,message_json['value']["attributes"]["pass"],device)
+        else:
+            print("No password found. No changed done.")
+    elif action == "things/twin/events/created": #create device in broker
+        if "pass" in message_json['value']["attributes"]:
+            print("Creating '" + mqtt_topic + "' with the password '" + message_json['value']["attributes"]["pass"] +"'...")
+            create_device_broker(mqtt_topic,message_json['value']["attributes"]["pass"],device)
+        else:
+            print("No password found! Setting '"+ mqtt_topic +"' password as 'default'")
+    
+    # add delete later on
+    else:
+        print("Message ignored")
+# aux = base64.b64encode("ditto".encode("ascii"))  -> encode to b64
+#----------------------------------------------------------------------------
+def update_device_broker(mqtt_topic,password,device):
+    # get mosquitto external container id
+    client = docker.from_env()
+    mosquitto_container = client.containers.list(all=True, filters={'name':'mosquitto_external'})
+
+    # open shared file used to encript password
+    f = open('auxiliar.passwd','w')
+    f.write(device+":"+password)
+    f.close()
+    
+    # Execute pass encription in external mosquitto container
+    pass_encript_command = "mosquitto_passwd -U mosquitto/config/auxiliar.passwd"
+    mosquitto_container[0].exec_run(pass_encript_command, tty=True, privileged=True)
+
+    # Update the encripted pass in the default config file
+    f = open('auxiliar.passwd','r')
+    pass_data = f.read()
+    f.close()
+
+    f =  open('mosquitto.passwd', 'r+')
+    file = f.read()
+    if device in file:
+        # print("found "+ device)
+        device_id_start = file.find(device)
+        device_id_end = file[device_id_start:].find("\n")
+        if device_id_end != -1: #not found \n, last item in the file
+            file_updated = file[:device_id_start] + pass_data + file[device_id_start+device_id_end+1:]
+            # print(file_updated)
+            f.seek(0)
+            f.write(file_updated)
+            f.truncate()
+            f.close()
+        else:
+            file_updated = file[:device_id_start] + pass_data
+            f.seek(0)
+            f.write(file_updated)
+            f.truncate()
+            f.close()       
+    else:
+        f.close()
+        print("Didn't find "+ device +" in 'mosquitto.passwd'. Adding it to the file...")
+        with open('mosquitto.passwd', 'a') as file:
+            file.write("\n"+pass_data[:-1])
+    
+    # Check if restrict topic in the default config file
+    f =  open('mosquitto.acl', 'r+')
+    file = f.read()
+    f.close()
+    if device in file:
+        # there could be something regarding updating the topic, but for this it should be done manualy
+        pass
+    else: # Topic not found, adding it to file
+        acl_data = "\n\nuser " + device +"\ntopic readwrite " + mqtt_topic
+        with open('mosquitto.acl', 'a') as file:
+            file.write(acl_data)
+
+    # Restarting container (mosquitto_external)
+    mosquitto_container[0].restart()
+    print('done')
+#----------------------------------------------------------------------------
+def create_device_broker(mqtt_topic,password,device): 
+     # get mosquitto external container id
+    client = docker.from_env()
+    mosquitto_container = client.containers.list(all=True, filters={'name':'mosquitto_external'})
+
+    # open shared file used to encript password
+    f = open('auxiliar.passwd','w')
+    f.write(device+":"+password)
+    f.close()
+    
+    # Execute pass encription in external mosquitto container
+    pass_encript_command = "mosquitto_passwd -U mosquitto/config/auxiliar.passwd"
+    mosquitto_container[0].exec_run(pass_encript_command, tty=True, privileged=True)
+
+    # Put the encripted pass in the default config file
+    f = open('auxiliar.passwd','r')
+    pass_data = f.read()
+    f.close()
+    with open('mosquitto.passwd', 'a') as file:
+        file.write("\n"+pass_data[:-1])
+
+    # Add device to .acl file
+    acl_data = "\n\nuser " + device +"\ntopic readwrite " + mqtt_topic
+    with open('mosquitto.acl', 'a') as file:
+        file.write(acl_data)
+
+    # Restarting container (mosquitto_external)
+    mosquitto_container[0].restart()
+    print('done')
+#----------------------------------------------------------------------------
+def main():
+    # Import config File
+    f = open('config.json')
+    config = json.load(f)
+
+    # Eternal mqtt loop
+    client = connect_mqtt(config)
+    subscribe(client,config)
+    client.loop_forever()
+
+
+if __name__=="__main__":
+    main()
