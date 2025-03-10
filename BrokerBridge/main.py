86029e7709f6ccd7de4cccc6da364774bb59ef0c
danycamarneiro <daniel.camarneiro@ua.pt>
2025-03-10 18:57:36 +0000

    adding message filtering to broker bridge

--- BrokerBridge/main.py
+++ BrokerBridge/main.py
@@ -1,9 +1,19 @@
 import paho.mqtt.client as mqtt
 import json
 import asyncio
+import requests
+import copy
 
 in_flag = False
 out_flag = False
+
+enable_telemetry = False
+enable_commands = False
+enable_configuration_requests = False
+enable_device_management = False
+ditto_hostname =""
+ditto_port = ""
+ditto_auth = ""
 
 # Inner broker
 def connect_mqtt_inner(configfile): 
@@ -73,18 +83,62 @@ def subscribe_out(client_in, client_out, configfile):
         message = msg.payload.decode()
         message_json = json.loads(message)
         add_check = False
-        # print("---Message Out---")
+        print("---Message Out---")
         # print(message_json)
-        if "check" in message_json:
-            if message_json["check"]:
-                pass
+        msg_topic_id = msg.topic[msg.topic.find("/")+1:]
+        ditto_topic = message_json["topic"]
+        aux = ditto_topic.find("/")
+        ditto_topic_id=ditto_topic[:aux]+":"+ditto_topic[aux+1:aux+1+ditto_topic[aux+1:].find("/")]
+
+        
+        if msg_topic_id == ditto_topic_id:
+            if "check" in message_json:
+                if message_json["check"]:
+                    pass
+                else:
+                    add_check = True
             else:
                 add_check = True
+            if add_check: # first time the message reaches que broker
+                message_action = ditto_topic[aux+2+ditto_topic[aux+1:].find("/"):]
+                message_action_command = message_action[:message_action.rfind("/")]
+
+                #--> add topic segmentation here
+                message_valid = False
+                if message_action == "things/twin/commands/modify" and enable_telemetry:
+                    print("telemetry")
+                    message_valid = True
+                elif message_action_command == "things/live/messages" and enable_commands:
+                    message_valid = True
+                    print("command")
+                elif message_action == "things/twin/commands/retrieve" and enable_configuration_requests:
+                    # --> add http forward method
+                    print("acquisition")
+                    ditto_url = "http://" + ditto_hostname + ":" + ditto_port + "/api/2/things/" +ditto_topic_id + message_json["path"]
+                    ditto_headers = {
+                            "Content-Type": "application/json",
+                            "Authorization": "Basic " + ditto_auth
+                        }
+                    data = requests.get(url=ditto_url, headers=ditto_headers)
+                    new_message_json = copy.deepcopy(message_json)
+                    new_message_topic = copy.deepcopy(msg.topic)
+                    new_message_json["value"]= json.loads(data.text)
+                    new_message_json["check"]= True
+                    print(new_message_json)
+                    
+                    client_out.publish(new_message_topic,json.dumps(new_message_json))
+                    pass
+                else: 
+                    if enable_device_management:
+                        print("management")
+                        message_valid = True
+
+                if message_valid:
+                    message_json["check"]= True
+                    client_in.publish(msg.topic,json.dumps(message_json))
         else:
-            add_check = True
-        if add_check:
-            message_json["check"]= True
-            client_in.publish(msg.topic,json.dumps(message_json))
+            pass
+            # print("error")
         
     client_out.subscribe(configfile["OutSubTopic"])
     client_out.on_message = on_message
@@ -94,6 +148,23 @@ async def main():
     f = open('InnerBridgeConfig.json')
     ConfigData = json.load(f)
     # print(ConfigData)
+    
+    # set message permitions
+    global enable_commands
+    global enable_configuration_requests
+    global enable_device_management
+    global enable_telemetry
+    global ditto_hostname
+    global ditto_auth
+    global ditto_port
+
+    enable_telemetry = ConfigData["Enable_telemetry"]
+    enable_commands =  ConfigData["Enable_commands"]
+    enable_configuration_requests =  ConfigData["Enable_configuration_requests"]
+    enable_device_management =  ConfigData["Enable_device_management"]
+    ditto_port = ConfigData["Ditto_port"]
+    ditto_hostname = ConfigData["Ditto_hostname"]
+    ditto_auth = ConfigData["Ditto_auth"]
 
     # Create connection with inner broker
     mqtt_inner = connect_mqtt_inner(ConfigData)
@@ -129,3 +200,7 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+## to add:
+# only allow devices to update status, retrieve info and answer command messages
+# add a custom feature to retrieve through http
